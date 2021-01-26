f7668b3f5f55b073912b3e48a5cff5a386ab2eb7
ReisAline <60517902+ReisAline@users.noreply.github.com>
2021-01-26 19:43:30 -0300

    imersao react + next

--- quis/pages/index.js
+++ quis/pages/index.js
@@ -1,17 +1,20 @@
-import styled from 'styled-components'
+import React from 'react';
+import Head from 'next/head';
+import styled from 'styled-components';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import {useRouter} from 'next/router';
 
 
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
+  padding-top: 45px
+  margin auto 10%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
@@ -19,16 +22,39 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const  [name, setName] = React.useState('');
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz sobre a Disney</title >
+      </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+          <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -38,7 +64,7 @@ export default function Home() {
 
             <p>aqui será um quiz sobre a Disney</p>
           </Widget.Content>
-        </Widget>
+        </Widget> 
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/ReisAline" />
---
+++ quis/pages/quiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function QustionarioQuiz(){
+  return(
+     <div>
+         Pagina de quiz.
+     </div>  
+  )
+}
